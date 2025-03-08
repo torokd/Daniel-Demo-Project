5053d5732e2028150a874a0a854c55d9e7abcf3a
Daniel Torok <torokdnl@gmail.com>
2025-03-08 15:27:19 +0100

    fontos valtozasok

--- pages/loginPageObjects.ts
+++ pages/loginPageObjects.ts
@@ -1,45 +1,52 @@
 import { type Locator, type Page } from "@playwright/test";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export class LoginPage {
   readonly page: Page;
 
-  readonly baseURL: string;
-  readonly password: string;
-  readonly standardUserName: string;
-  readonly blockedUserName: string;
+  readonly variables: {
+    baseURL: string;
+    password: string;
+    standardUserName: string;
+    blockedUserName: string;
+    loginErrorMessage: string;
+  };
 
   readonly locators: {
     userNameInput: Locator;
     passwordInput: Locator;
     loginButton: Locator;
+    hamburgerMenuIcon: Locator;
+    loginError: Locator;
   };
 
   constructor(page: Page) {
     this.page = page;
     
-    this.baseURL = process.env.BASE_URL || '';
-    this.password = process.env.PASSWORD || '';
-    this.standardUserName = process.env.STANDARD_USER_NAME || '';
-    this.blockedUserName = process.env.BLOCKED_USER_NAME || '';
+    this.variables = {
+      baseURL: "https://www.saucedemo.com/",
+      password: "secret_sauce",
+      standardUserName: "standard_user",
+      blockedUserName: "locked_out_user",
+      loginErrorMessage: "Epic sadface: Sorry, this user has been locked out.",
+    };
 
     this.locators = {
-      userNameInput: page.locator('text=Username'),
-      passwordInput: page.locator('text=Password'),
-      loginButton: page.locator('text=Login'),
+      userNameInput: page.getByRole('textbox', { name: 'Username' }),
+      passwordInput: page.getByRole('textbox', { name: 'Password' }),
+      loginButton: page.getByRole('button', { name: 'Login' }),
+      hamburgerMenuIcon: page.getByRole('button', { name: 'Open Menu' }),
+      loginError: page.getByText(this.variables.loginErrorMessage),
     }
     
   };
 
-  async goToLoginPage() {
-    await this.page.goto(this.baseURL);
+  async goToMainPage() {
+    await this.page.goto(this.variables.baseURL);
   }
 
   async login(userName) {
     await this.locators.userNameInput.fill(userName);
-    await this.locators.passwordInput.fill(this.password);
+    await this.locators.passwordInput.fill(this.variables.password);
     await this.locators.loginButton.click();
   }
 };
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,43 +5,20 @@ import { LoginPage, GeneralHelperPage } from "../pages/pageHub";
 
 let loginPage: LoginPage;
 let generalHelperPage: GeneralHelperPage;
-//https://parabank.parasoft.com/parabank/about.htm;jsessionid=4803A87F0082657195F8E9299912F569
-//https://github.com/torokd/Daniel-Demo-Project
 
 test.beforeEach(async ({ page }) => {
   loginPage = new LoginPage(page);
   generalHelperPage = new GeneralHelperPage(page);
 });
 
-test.describe(
-  "Login tests",
-  {
-    tag: ["@Regression", "@Sanity", "@Login"],
-    annotation: { type: "valami", description: "leiras" },
-  },
-  () => {
-    test.only("Successful login with standard user", async (page) => {
-      // Megnyitja a fooldalt
-      await loginPage.goToLoginPage();
+test("Successful login with standard user", async () => {
+  await loginPage.goToMainPage();
+  await loginPage.login(loginPage.variables.standardUserName);
+  await expect(loginPage.locators.hamburgerMenuIcon).toBeVisible();
+});
 
-      // Kitolti a usernamet, passwordot a megfelelo adatokkal es megnyomja a login gombot
-      await loginPage.login(loginPage.standardUserName);
-
-      await generalHelperPage.screenshot();
-
-      // Ravizsgal, hogy megvan-e a belepes utani screenen egy icon
-      await expect(loginPage.locators.hamburgerMenuIcon).toBeVisible();
-
-    });
-
-    test("Unsuccessful login with blocked user", async () => {
-      await loginPage.goToLoginPage();
-
-      await loginPage.login(loginPage.blockedUserName);
-
-      await expect(
-        loginPage.locators.loginErrorMessage
-      ).toHaveText('Epic sadface: Sorry, this user has been locked out.');
-    });
-  }
-);
+test("Unsuccessful login with blocked user", async () => {
+  await loginPage.goToMainPage();
+  await loginPage.login(loginPage.variables.blockedUserName);
+  await expect(loginPage.locators.loginError).toHaveText(loginPage.variables.loginErrorMessage);
+});
